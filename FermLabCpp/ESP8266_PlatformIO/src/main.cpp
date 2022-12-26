1e4a844683b8e6a842baedff44dfea5066816cd2
labinho <labinho@gmx.de>
2022-12-26 14:07:47 +0100

    update ESP8266 main.cpp
    generate_random_payload(): get timestamp and increase sequence number every 1 second

--- FermLabCpp/ESP8266_PlatformIO/src/main.cpp
+++ FermLabCpp/ESP8266_PlatformIO/src/main.cpp
@@ -11,6 +11,8 @@
 
 #include "credentials.h"
 
+extern char *tzname[2];
+
 org_eclipse_tahu_protobuf_Payload payload = org_eclipse_tahu_protobuf_Payload_init_zero;
 
 void setup_wifi() {
@@ -33,18 +35,22 @@ void setup_wifi() {
 
 time_t setDateTime() {
   configTime(TZ_Europe_Berlin, "pool.ntp.org", "time.nist.gov");
-  Serial.print("Waiting for NTP time sync: ");
   time_t now = time(nullptr);
   while (now < 8 * 3600 * 2) {
     delay(100);
-    Serial.print(".");
     now = time(nullptr);
   }
-  Serial.println();
   struct tm timeinfo;
   gmtime_r(&now, &timeinfo);
-  Serial.printf("%s %s", tzname[0], asctime(&timeinfo));
   return now;
+}
+
+uint64_t getSeqNo() {
+  if (payload.seq == 256) {
+    payload.seq = 0;
+  }
+  payload.seq++;
+  return payload.seq;
 }
 
 void setup() {
@@ -55,17 +61,36 @@ void setup() {
   digitalWrite(LED_BUILTIN, HIGH);
   setup_wifi();
   time_t current_time = setDateTime();
+
   payload.has_timestamp = true;
   payload.timestamp = current_time;
+
   payload.has_seq = true;
   payload.seq = 0;
 }
 
+void generate_random_payload() {
+
+  payload.timestamp = setDateTime();
+  payload.seq = getSeqNo();
+
+  Serial.print("payload [");
+
+  Serial.print("timestamp=");
+  Serial.print(payload.timestamp);
+  Serial.print(", ");
+  
+  Serial.print("seq=");
+  Serial.print(payload.seq);
+  Serial.println("]");
+}
+
 void loop() {
-  Serial.println(payload.timestamp);
+  
   digitalWrite(LED_BUILTIN, LOW);
   delay(1000);
   digitalWrite(LED_BUILTIN, HIGH);
   delay(1000);
-  payload.timestamp = payload.timestamp + 2;
+
+  generate_random_payload();
 }
